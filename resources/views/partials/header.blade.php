08c933549d0fea064cb2a31b867128630a9336dc
Guido <guido.amodio@gmail.com>
2020-10-12 17:32:05 +0200

    content

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,19 +1,30 @@
+<div class="g-topbar container-fluid">
+    <div class="g-notification">
+        <img src="https://www.boolean.careers/images/icons/bell.png" alt="bell">
+        <span>prossimo corso in partenza</span>
+        <span class="g-date">13 Gennaio</span>
+    </div>
+</div>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="{{route('StaticPage.home')}}">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="{{route('StaticPage.chisiamo')}}">chi siamo</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="{{route('Student.studenti')}}">studenti</a>
-      </li>
-    </ul>
-  </div>
+    <div class="container">
+        <img class="logo" src="https://www.boolean.careers/images/misc/logo.png" alt="logo boolean">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto d-flex align-items-center">
+                <li class="nav-item active">
+                <a class="" href="{{route('StaticPage.home')}}">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                <a class="" href="{{route('StaticPage.chisiamo')}}">chi siamo</a>
+                </li>
+                <li class="nav-item">
+                <a class="" href="{{route('Student.studenti')}}">studenti</a>
+                </li>
+                <button type="button" class="button1">Candidati Ora</button>
+            </ul>
+        </div>
+    </div>
 </nav>
--- resources/views/studenti.blade.php
+++ resources/views/studenti.blade.php
@@ -3,20 +3,24 @@
 @section('title', 'Studenti')
 
 @section('content')
-    <div class="card-group">
-        @foreach ($data as $student)
-            <div class="g-card shadow">
+    <div class="container">
 
-                <div class="g-card-top">
-                    <img src="{{$student['img']}}" class="g-card-img" alt="{{$student['nome']}}">
-                    <h5 class="g-card-name">{{$student['nome']}} ({{$student['anni']}} Anni)</h5>
-                    <p class="g-card-hire">Assunto da {{$student['azienda']}}</p>
-                </div>
-                <div class="g-card-bottom">
-                    <p class="g-card-description">{{$student['descrizione']}}</p>
 
+        <div class="card-group">
+            @foreach ($data as $student)
+                <div class="g-card shadow">
+
+                    <div class="g-card-top">
+                        <img src="{{$student['img']}}" class="g-card-img" alt="{{$student['nome']}}">
+                        <h5 class="g-card-name">{{$student['nome']}} ({{$student['anni']}} Anni)</h5>
+                        <p class="g-card-hire">Assunto da {{$student['azienda']}}</p>
+                    </div>
+                    <div class="g-card-bottom">
+                        <p class="g-card-description">{{$student['descrizione']}}</p>
+
+                    </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
 @endsection
